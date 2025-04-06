dbec4aff5d12829877896ccb0b1120268b75c4b8
Jeremy <jeremywhitenect@gmail.com>
2025-04-06 10:22:09 -0300

    Remove uneeded code

--- J2EEFinalProject/src/main/java/com/example/j2eefinalproject/controller/MainController.java
+++ J2EEFinalProject/src/main/java/com/example/j2eefinalproject/controller/MainController.java
@@ -1,8 +1,5 @@
 package com.example.j2eefinalproject.controller;
 
-import com.example.j2eefinalproject.model.User;
-import com.example.j2eefinalproject.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +9,6 @@ import java.security.Principal;
 @Controller
 @RequestMapping("/")
 public class MainController {
-
-    @Autowired
-    private UserService userService;
 
     // Get index page
     @GetMapping
